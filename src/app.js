cb9719ccba5c86813e88ea83aca82aac76e64712
samuelpampi <samuelpampillonroa@gmail.com>
2025-02-19 19:07:34 +0100

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -1,1 +1,61 @@
-console.log('Hello, World!');
+const {casco, guantes, vendas, botas, bucales} = require('./utils.js');
+var total = 0;
+
+/*Cargar las opciones de los select de forma dinamica*/
+function cargarProductos(){
+    cargarOpciones('helmet', casco);
+    cargarOpciones('gloves', guantes);
+    cargarOpciones('vendas', vendas);
+    cargarOpciones('botas', botas);
+    cargarOpciones('bucal', bucales);
+}
+
+function cargarOpciones(selectedId, productos){
+    let select = document.getElementById(selectedId);
+    if(select){
+        for(let [nombre, precio] of Object.entries(productos)){
+            let option = document.createElement('option');
+            option.text = `${nombre} - ${precio}€`;
+            option.value = nombre;
+            select.appendChild(option);
+        }
+
+        //Agreamos el evento para detectar cada cambio del select
+        select.addEventListener("change", (event) => {
+            actualizarSeleccion(selectedId, event.target.value, productos[event.target.value]);    
+        });
+    }
+}
+
+/*Actualiza la lista de los elementos seleccionados*/
+function actualizarSeleccion(selectedId, producto, precio){
+    let selectedElement = document.getElementById(`selected-${selectedId}`);
+    if(selectedElement){
+        if(producto != '0'){
+            selectedElement.innerHTML = `${producto} <span>${precio}€</span>`;
+        } else{
+            selectedElement.innerHTML = ''; 
+        }
+        
+    }
+    calcularTotal(); //Llamamos a calcularTotal cada vez que se seleccione un producto
+}
+
+/* Calcula el precio total */
+function calcularTotal() {
+    total = 0;
+    totalElement = document.getElementById('total');
+
+    //Recorrer todos los elementos seleccionados y sumar los precios
+    document.querySelectorAll(".selected-option span").forEach(span => {
+        let precio = parseFloat(span.textContent.replace('€', '')) || 0;
+        total += precio;
+    });
+
+    totalElement.innerHTML = `${total}€`;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarProductos();
+});
---
+++ src/utils.js
@@ -0,0 +1,37 @@
+const guantes={
+    "Venum Giant 3.0":75,
+    "Everlast Core 2":40,
+    "Fairtex BGV9": 120,
+    "Grant Proffesional": 700
+};
+
+const casco={
+    "BENLEE Rocky Marciano": 40,
+    "Venum Challenger 2.0": 60,
+    "Leone 11947": 129,
+    "Hayabusa T3": 160
+};
+
+const vendas={
+    "Leone 1947 2,5m": 7,
+    "Farabi 4,5m": 10,
+    "BENLEE Rocky Marciano 5m": 15,
+    "ROSTER 4,5m": 9
+}
+
+const botas={
+    "Leone 1947 CL110": 50,
+    "Adidas Box Hog 3": 80,
+    "Venum Elite": 100,
+    "Nike HyperKO": 150
+}
+
+
+const bucales={
+    "Venum Challenger 2.0": 15,
+    "Shock Doctor Gel Max": 20,
+    "Everlast Single": 25,
+    "SISU Aero": 30
+}
+
+module.exports = { guantes, casco, vendas, botas, bucales };
